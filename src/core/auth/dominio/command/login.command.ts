6ab736a06c2bfd30f51077058954c520b2142f5f
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-11-17 21:48:14 -0300

    swagger

--- src/core/auth/dominio/command/login.command.ts
+++ src/core/auth/dominio/command/login.command.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginCommand {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   senha: string;
 }
