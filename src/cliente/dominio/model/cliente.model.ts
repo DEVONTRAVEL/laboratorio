dac1023601a7d4c9e0f402eeee7e48089eb5667d
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-11-19 12:37:57 -0300

    models

--- src/cliente/dominio/model/cliente.model.ts
+++ src/cliente/dominio/model/cliente.model.ts
@@ -1,5 +1,8 @@
+import { Dentista } from 'src/dentista/dominio/model/dentista.model';
+
 export class Cliente {
   id: string;
   nome: string;
   cpf: number;
+  dentista?: Dentista[];
 }
--- src/dentista/dominio/model/dentista.model.ts
+++ src/dentista/dominio/model/dentista.model.ts
@@ -1,5 +1,8 @@
+import { Cliente } from 'src/cliente/dominio/model/cliente.model';
+
 export class Dentista {
   id: string;
   nome: string;
   clienteId: string;
+  cliente?: Cliente;
 }
