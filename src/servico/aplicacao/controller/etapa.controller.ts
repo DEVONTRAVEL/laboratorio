7d98d80db63b50c4044a7d863beb6b413b6b18c7
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-11-25 22:11:55 -0300

    jwt

--- src/servico/aplicacao/controller/etapa.controller.ts
+++ src/servico/aplicacao/controller/etapa.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/core/auth/guard/jwt.guard';
 import { CriarEtapaCommand } from 'src/servico/dominio/command/criarEtapa.command';
 import { Etapa } from 'src/servico/dominio/model/etapa.model';
 import { EtapaService } from '../service/etapa.service';
 
 @ApiTags('Etapa')
-// @UseGuards(JwtGuard)
+@UseGuards(JwtGuard)
 @ApiBearerAuth()
 @Controller('etapa')
 export class EtapaController {
